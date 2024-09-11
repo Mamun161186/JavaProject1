0a3f2ce8f84d846399f0e95632175427f4857f0e
Abdullah Al Mamun <almamun161186@gmail.com>
2024-09-11 19:35:15 +0600

    This is first class assignment

---
+++ src/SQA_Class1_Package/fisrtJavaProgram.java
@@ -0,0 +1,7 @@
+package SQA_Class1_Package;
+
+public class fisrtJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/SQA_Class1_Package/secondJavaProgram.java
@@ -0,0 +1,14 @@
+package SQA_Class1_Package;
+
+public class secondJavaProgram {
+
+    public void add(){
+        float sum;
+    }
+    public static void main(String[] args) {
+        int number1;
+        float number2;
+
+    }
+
+}
