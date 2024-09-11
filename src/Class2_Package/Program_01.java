53db9af56d975c509b02b9fb00022c0b8ab71da4
Abdullah Al Mamun <almamun161186@gmail.com>
2024-09-11 19:54:12 +0600

    add the java 2nd assignment with a branch

---
+++ src/Class2_Package/Program_01.java
@@ -0,0 +1,25 @@
+package Class2_Package;
+
+public class Program_01 {
+    //Instance variable
+    char name = 'm';
+    int t = 5;
+    //Static variable
+    static float number = 3.54f;
+
+    public static void main(String[] args) {
+        //Local variable
+        double count = 5.9;
+        String k = "mamun";
+        Program_01 p = new Program_01();
+
+        System.out.println(Program_01.number);
+        System.out.println(count);
+       // System.out.println(t);
+        System.out.println(number);
+        System.out.println(number);
+
+
+    }
+
+}
---
+++ src/Class2_Package/Program_04.java
@@ -0,0 +1,19 @@
+package Class2_Package;
+
+public class Program_04 {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7,sum;
+        sum = num1 * num2;
+        System.out.println(sum);
+        sum = sum * sum;
+        System.out.println(sum);
+        num2 = num2 - num1;
+        System.out.println(num2);
+        num2 = sum / num2;
+        System.out.println(num2);
+        sum = sum % num2;
+        System.out.println(sum);
+
+    }
+}
