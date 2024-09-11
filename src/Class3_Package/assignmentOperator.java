0d00f67f1172064a690793aede8868792796cecc
Abdullah Al Mamun <almamun161186@gmail.com>
2024-09-11 20:30:39 +0600

    add the java 3rd assignment with a branch

---
+++ src/Class3_Package/assignmentOperator.java
@@ -0,0 +1,12 @@
+package Class3_Package;
+
+public class assignmentOperator {
+    public static void main(String[] args) {
+        int a = 9, b = 8;
+        System.out.println( a+=b);
+        System.out.println( a-=b);
+        System.out.println( a*=b);
+        System.out.println( a/=b);
+
+    }
+}
