6f24fb24ee5105159fc92c2ca41d7d7028c2735b
Abdullah Al Mamun <almamun161186@gmail.com>
2024-09-14 16:03:17 +0600

    commit3

--- src/Class4_Package/JumpStatement.java
+++ src/Class4_Package/JumpStatement.java
@@ -1,4 +1,26 @@
 package Class4_Package;
 
 public class JumpStatement {
+    public static void main(String[] args) {
+        System.out.println("workflow of break");
+        int i=0;
+        for (i=1;i<=100;i++){
+            if(i==5)
+                break;
+            System.out.println(i);
+
+        }
+        System.out.println("workflow of continue");
+
+        int j=0;
+        for(j=1;j<=10;j++){
+            if(j==5)
+                continue;
+            System.out.println(j);
+        }
+
+
+        }
+
+
 }
